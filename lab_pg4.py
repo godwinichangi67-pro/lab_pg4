68367c13629c10ab4981338da4c4b62d53d2f52a
Godwin <godwinichangi67@gmail.com>
2026-09-15 12:47:47 +0530

    Saving the exisitng file'

--- lab_pg4.py
+++ lab_pg4.py
@@ -3,6 +3,13 @@ b = int(input("Enter second number: "))
 
 addition = a + b
 subtraction = a - b
+multiplication = a*b
 
 print("Addition =", addition)
 print("Subtraction =", subtraction)
+print("Multiplication =",multiplication)
+if b!=0:
+    division = a/b
+    print("Division =",division)
+else:
+    print("Division is not possible because denominator is zero")
